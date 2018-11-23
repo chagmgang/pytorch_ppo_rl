776977742c02d08428cb928feb0b04265995f046
ckg <chagmgang@gmail.com>
2018-11-23 16:00:19 +0900

    add

--- model.py
+++ model.py
@@ -10,47 +10,6 @@ class Flatten(nn.Module):
     def forward(self, input):
         return input.view(input.size(0), -1)
 
-class CNNRNDActorCriticNetwork(nn.Module):
-    def __init__(self):
-        super(CNNRNDActorCriticNetwork, self).__init__()
-        self.conv1 = nn.Conv2d(4, 32, 8, stride=4)
-        self.conv2 = nn.Conv2d(32, 64, 4, stride=2)
-        self.conv3 = nn.Conv2d(64, 128, 3, stride=1)
-        self.fc1 = nn.Linear(7*7*128, 7*128)
-        self.fc2 = nn.Linear(7*128, 256)
-
-        self.actor = nn.Linear(256, 18)
-        self.critic_ex = nn.Linear(256, 1)
-        self.critic_in = nn.Linear(256, 1)
-
-    def forward(self, x):
-        x = F.relu(self.conv1(x))
-        x = F.relu(self.conv2(x))
-        x = F.relu(self.conv3(x))
-        x = x.view(-1, 7*7*128)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        policy = self.actor(x)
-        value_ex = self.critic_ex(x)
-        value_in = self.critic_in(x)
-        return policy, value_ex, value_in
-
-class CnnRND(nn.Module):
-    def __init__(self):
-        super(CnnRND, self).__init__()
-        self.conv1 = nn.Conv2d(1, 32, 8, stride=4)
-        self.conv2 = nn.Conv2d(32, 64, 4, stride=2)
-        self.conv3 = nn.Conv2d(64, 128, 3, stride=1)
-        self.fc1 = nn.Linear(7*7*128, 7*128)
-        
-    def forward(self, x):
-        x = F.relu(self.conv1(x))
-        x = F.relu(self.conv2(x))
-        x = F.relu(self.conv3(x))
-        x = x.view(-1, 7*7*128)
-        x = F.relu(self.fc1(x))
-        return x
-
 class MlpActorCriticNetwork(nn.Module):
     def __init__(self, input_size, output_size):
         super(MlpActorCriticNetwork, self).__init__()
